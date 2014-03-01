b11d703b93ccf807707bab5cdbe9199a42dd168f
Shashi Essp <shashiessp@gmail.com>
2014-03-02 04:05:36 +0530

    added specific task method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask import abort
 
 app = Flask(__name__)
 
@@ -18,9 +19,13 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
-def get_tasks():
-    return jsonify( { 'tasks': tasks } )
+# @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        abort(404)
+    return jsonify( { 'task': task[0] } )
 
 if __name__ == '__main__':
     app.run(debug = True)
